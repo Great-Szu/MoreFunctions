04eb76dfc5894ff2adf7d72bb6071535cc974cdf
Great-Szu <mateusz.chol.net@outlook.com>
2025-07-10 22:18:54 +0200

    Day 4, Protected functions (__function)

--- import_test.py
+++ import_test.py
@@ -1,5 +1,12 @@
 import blackJack
 
-print(__name__)
-blackJack.play()
-print(blackJack.cards)
+g = sorted(globals())
+for x in g:
+    print(x)
+
+
+# print(__name__)
+#
+# blackJack.deal_card(blackJack.dealer_card_frame)
+# blackJack.play()
+# print(blackJack.cards)
